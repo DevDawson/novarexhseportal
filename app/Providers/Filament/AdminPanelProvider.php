473cd4082638267322bdebf9532b02aaa474b336
DevDawson <dauxonphilip@gmail.com>
2026-06-13 15:39:17 +0300

    new data

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -42,11 +42,9 @@ class AdminPanelProvider extends PanelProvider
                 Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+
             ->widgets([
                 Widgets\AccountWidget::class,
-                // Widgets\FilamentInfoWidget::class,
-            ])
-            ->widgets([
                 StatsOverview::class,
                 RevenueVsExpensesChart::class,
                 IncidentSeverityChart::class,
